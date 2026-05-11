9216f9518e76d88477df303ff68bf1cbba6a4663
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2026-05-11 08:50:37 +0200

    naive evaluation function

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 CC=gcc
+DEPS=board.c piece.c square.c move.c
 
 .PHONY: all clean
 
-all: chess, tests
+all: chess tests
 
-tests: tests.c board.c piece.c square.c move.c
-	$(CC) tests.c board.c piece.c square.c move.c -o ./bin/tests
+tests: tests.c $(DEPS)
+	$(CC) tests.c $(DEPS) -o ./bin/tests
 	./bin/tests
 
-chess:
-	$(CC) main.c -o bin/main
+chess: main.c $(DEPS)
+	$(CC) main.c  $(DEPS) -o bin/main
 	./bin/main
 
 clean:
--- main.c
+++ main.c
@@ -1,16 +1,53 @@
+#include <stdio.h>
 
+#include "board.h"
 
+#define PAWN_VAL 100
+#define KNIGHT_VAL 300
+#define BISHOP_VAL 300
+#define ROOK_VAL 500
+#define QUEEN_VAL 900
+#define KING_VAL 10000
 
+int count_material(board_t* board, color_t color)
+{
+    int total = 0;
 
+    for (int i = 0; i < FILES * RANKS; i++)
+    {
+        piece_t p = board->squares[i];
+        if (piece_color(p) != color) continue;
 
+        switch (piece_type(p))
+        {
+            case PAWN: total += PAWN_VAL; break;
+            case KNIGHT: total += KNIGHT_VAL; break;
+            case BISHOP: total += BISHOP_VAL; break;
+            case ROOK: total += ROOK_VAL; break;
+            case QUEEN: total += QUEEN_VAL; break;
+            // case KING: total += KING_VAL; break;
+            default: break;
+        }
+    }
+
+    return total;
+}
+
+int evaluate(board_t* board)
+{
+    int w_mat = count_material(board, WHITE);
+    int b_mat = count_material(board, BLACK);
+
+    return w_mat - b_mat;
+}
 
 int main()
 {
-    // chess_t state = {0};
-    // chess_init(&state);
-    // chess_print(&state);
-
-    // printf("%c\n", piece_string(board_at_square(state, E2)));
-
+    board_t board = {0};
+    board_init(&board);
+    printf("material for white = %d\n", count_material(&board, WHITE));
+    printf("material for black = %d\n", count_material(&board, BLACK));
+    printf("position evaluation = %d\n", evaluate(&board));
+    
     return 0;
 }
